c1472cfb7eea78a3cddbfc9180eaaf61a219fa9c
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-10-28 15:30:51 -0700

    Use combine and don't unquote
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- lexer.py
+++ lexer.py
@@ -2,17 +2,17 @@ from pyparsing import *
 
 node_name = Word(alphanums + ",.-+_") ^ Literal("/")
 decimal_int = Word(nums)
-hex_int = Literal("0x") + Word(hexnums)
+hex_int = Combine(Literal("0x") + Word(hexnums))
 integer = decimal_int ^ hex_int
 unit_address = Word(hexnums)
 property_name = Word(alphanums + ",.-_+?#")
 label = Word(alphanums + "_")
-label_creation = label + Literal(":")
-string = dblQuotedString
+label_creation = Combine(label + Literal(":"))
+string = QuotedString(quoteChar='"', unquoteResults=False)
 stringlist = delimitedList(string)
-node_path = Literal("/") + delimitedList(node_name, delim="/") + Optional(Literal("@") + unit_address)
-reference = Literal("&") + ((Literal("{") + node_path + Literal("}")) ^ label)
-directive = QuotedString(quoteChar="/") + Optional(property_name ^ node_name ^ reference ^ (integer * 2)) + Literal(";")
+node_path = Combine(Literal("/") + delimitedList(node_name, delim="/") + Optional(Literal("@") + unit_address))
+reference = Combine(Literal("&") + ((Literal("{") + node_path + Literal("}")) ^ label))
+directive = QuotedString(quoteChar="/", unquoteResults=False) + Optional(property_name ^ node_name ^ reference ^ (integer * 2)) + Literal(";")
 
 # todo: arithmetic expressions
 arith_expr = integer
@@ -21,7 +21,7 @@ array = Literal("<") + ZeroOrMore(arith_expr ^ string ^ reference ^ label_creati
 bytestring = Literal("[") + (Word(hexnums) ^ label_creation) + Literal("]")
 property_assignment = property_name + Optional(Literal("=") + (array ^ bytestring ^ stringlist ^ label_creation)) + Literal(";")
 
-node_opener = Optional(label_creation) + node_name + Optional(Literal("@") + unit_address) + Literal("{")
+node_opener = Optional(label_creation) + Combine(node_name + Optional(Literal("@") + unit_address)) + Literal("{")
 node_closer = Literal("}") + Literal(";")
 node_definition = nestedExpr(node_opener, node_closer, content=(property_assignment ^ directive))
 
