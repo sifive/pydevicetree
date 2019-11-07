f4c14c873ee27e1c502d0a7cc9c8f6efc0d41ba8
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-11-07 12:06:17 -0800

    Document Property methods
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- pydevicetree/ast/property.py
+++ pydevicetree/ast/property.py
@@ -8,6 +8,7 @@ from pydevicetree.ast.helpers import wrapStrings, formatLevel
 
 class PropertyValues:
     def __init__(self, values: List[Any] = None):
+        """Create a PropertyValue"""
         self.values = values
 
     def __repr__(self) -> str:
@@ -23,6 +24,7 @@ class PropertyValues:
         return len(self.values)
 
     def to_dts(self, formatHex: bool = False) -> str:
+        """Format the values in Devicetree Source format"""
         return " ".join(wrapStrings(self.values, formatHex))
 
     def __getitem__(self, key) -> Any:
@@ -35,36 +37,43 @@ class PropertyValues:
 
 class Bytestring(PropertyValues):
     def __init__(self, bytelist: List[int] = None):
+        """Create a Bytestring object"""
         PropertyValues.__init__(self, bytearray(bytelist))
 
     def __repr__(self) -> str:
         return "<Bytestring " + str(self.values) + ">"
 
     def to_dts(self, formatHex: bool = False) -> str:
+        """Format the bytestring in Devicetree Source format"""
         return "[" + " ".join("%02x" % v for v in self.values) + "]"
 
 class CellArray(PropertyValues):
     def __init__(self, cells: List[Any] = None):
+        """Create a CellArray object"""
         PropertyValues.__init__(self, cells)
 
     def __repr__(self) -> str:
         return "<CellArray " + self.values.__repr__() + ">"
 
     def to_dts(self, formatHex: bool = False) -> str:
+        """Format the cell array in Devicetree Source format"""
         return "<" + " ".join(wrapStrings(self.values, formatHex)) + ">"
 
 class StringList(PropertyValues):
     def __init__(self, strings: List[str] = None):
+        """Create a StringList object"""
         PropertyValues.__init__(self, strings)
 
     def __repr__(self) -> str:
         return "<StringList " + self.values.__repr__() + ">"
 
     def to_dts(self, formatHex: bool = False) -> str:
+        """Format the list of strings in Devicetree Source format"""
         return ", ".join(wrapStrings(self.values))
 
 class Property:
     def __init__(self, name: str, values: PropertyValues):
+        """Create a Property object"""
         self.name = name
         self.values = values
 
@@ -75,6 +84,7 @@ class Property:
         return self.to_dts()
 
     def to_dts(self, level: int = 0) -> str:
+        """Format the Property assignment in Devicetree Source format"""
         if self.name in ["reg", "ranges"]:
             value = self.values.to_dts(formatHex=True)
         else:
