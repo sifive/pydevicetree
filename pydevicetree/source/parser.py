b28a2b0eccff75ab2008de7270e66ca04147b0fc
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-11-07 12:13:15 -0800

    Document parser functions
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- pydevicetree/source/parser.py
+++ pydevicetree/source/parser.py
@@ -8,6 +8,7 @@ from pydevicetree.source import grammar
 from pydevicetree.ast import *
 
 def transformNode(string, location, tokens):
+    """Transforms a ParseResult into a Node"""
     properties = [e for e in tokens.asList() if isinstance(e, Property)]
     directives = [e for e in tokens.asList() if isinstance(e, Directive)]
     children = [e for e in tokens.asList() if isinstance(e, Node)]
@@ -19,6 +20,7 @@ def transformNode(string, location, tokens):
                 directives=directives, children=children)
 
 def transformPropertyAssignment(string, location, tokens):
+    """Transforms a ParseResult into a Property"""
     for v in tokens.value:
         if isinstance(v, PropertyValues):
             return Property(tokens.property_name, v)
@@ -30,27 +32,33 @@ def transformPropertyAssignment(string, location, tokens):
     return Property(tokens.property_name, PropertyValues([]))
 
 def transformDirective(string, location, tokens):
+    """Transforms a ParseResult into a Directive"""
     return Directive(tokens.directive, tokens.option)
 
 def evaluateArithExpr(string, location, tokens):
+    """Evaluates a ParseResult as a python expression"""
     flat_tokens = list(chain.from_iterable(tokens.asList()))
     expr = " ".join(str(t) for t in flat_tokens)
     # pylint: disable=eval-used
     return eval(expr)
 
 def transformTernary(string, location, tokens):
+    """Evaluates a ParseResult as a ternary expression"""
     # pylint: disable=eval-used
     return eval(str(tokens[2]) +" if " + str(tokens[0]) + " else " + str(tokens[4]))
 
 def transformPropertyValues(string, location, tokens):
+    """Transforms a ParseResult into a PropertyValues"""
     if len(tokens.asList()) == 1:
         return tokens.asList()[0]
     return PropertyValues(tokens.asList())
 
 def transformStringList(string, location, tokens):
+    """Transforms a ParseResult into a StringList"""
     return StringList(tokens.asList())
 
 def transformBytestring(string, location, tokens):
+    """Transforms a ParseResult into a Bytestring"""
     inttokens = []
     for t in tokens.asList():
         if all(c in "0123456789abcdefABCDEF" for c in t):
@@ -58,6 +66,7 @@ def transformBytestring(string, location, tokens):
     return Bytestring(inttokens)
 
 def transformCellArray(string, location, tokens):
+    """Transforms a ParseResult into a CellArray"""
     return CellArray(tokens.asList())
 
 grammar.node_definition.setParseAction(transformNode)
@@ -71,6 +80,7 @@ grammar.cell_array.setParseAction(transformCellArray)
 grammar.property_values.setParseAction(transformPropertyValues)
 
 def printTree(tree, level=0):
+    """Helper function to print a bunch of elements as a tree"""
     def printlevel(level, s):
         print(" " * level + s)
 
@@ -102,12 +112,14 @@ def printTree(tree, level=0):
                 printlevel(level, "Directive %s" % item.directive)
 
 def parentNodes(tree, parent=None):
+    """Walks a tree and sets Nodes' parent field to point at their parent"""
     for item in tree:
         if isinstance(item, Node):
             item.parent = parent
             parentNodes(item.children, item)
 
 def recurseIncludeFiles(elements, pwd):
+    """Recursively follows and parses /include/ directives an a tree"""
     for e in elements:
         if isinstance(e, Directive):
             if e.directive == "/include/":
@@ -121,6 +133,7 @@ def recurseIncludeFiles(elements, pwd):
                 elements += parseElements(contents)
 
 def parseElements(dts, pwd="", followIncludes=False):
+    """Parses a string into a list of elements"""
     elements = grammar.devicetree.parseString(dts)
     parentNodes(elements)
     if followIncludes:
@@ -128,6 +141,7 @@ def parseElements(dts, pwd="", followIncludes=False):
     return elements
 
 def parseTree(dts, pwd="", followIncludes=False):
+    """Parses a string into a full Devicetree"""
     return Devicetree(parseElements(dts, pwd, followIncludes))
 
 if __name__ == "__main__":
