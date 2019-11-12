9c1f06c1f9724944ffd49e7c6ec7cb5030f0b37b
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-11-12 10:24:47 -0800

    Add autodoc extension
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../pydevicetree'))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,6 +33,7 @@ with open("../../setup.py", "r") as setup_py:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
